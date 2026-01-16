c180c0f21e3d2f2d3080c94481750976f7477fa6
Pavel Prigoda <rotares@disroot.org>
2026-01-16 15:50:43 +0300

    feat(taskList): add v-if render

--- src/components/taskList.vue
+++ src/components/taskList.vue
@@ -1,20 +1,17 @@
 <script setup>
-import taskItem from '@/components/taskItem.vue';
-import {useTaskStore} from '@/stores/task'
+import taskItem from '@/components/taskItem.vue'
+import { useTaskStore } from '@/stores/task'
 import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 
 const store = useTaskStore()
-const {taskItems} = storeToRefs(store)
-
+const { taskItems } = storeToRefs(store)
 </script>
 
 <template>
-  <div class="max-w-2xl mx-auto flex flex-col gap-5">
+  <div v-if="taskItems.length > 0" class="max-w-2xl mx-auto flex flex-col gap-5">
     <taskItem v-for="task in taskItems" :task="task"></taskItem>
   </div>
 </template>
 
-<style scoped>
-  
-</style>
+<style scoped></style>
