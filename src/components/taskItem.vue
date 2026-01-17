fc5020c3ae34a4843af051a5924119790862116e
Pavel Prigoda <rotares@disroot.org>
2026-01-17 14:14:35 +0300

    feat(taskItem): add destroy action and new styles

--- src/components/taskItem.vue
+++ src/components/taskItem.vue
@@ -1,32 +1,51 @@
 <script setup>
-import { ref } from 'vue';
-  const props = defineProps({
-    task: {
-      type: Object,
-      required: true,
-      default: {}
-    }
-  })
+import { ref } from 'vue'
+import { useTaskStore } from '@/stores/task'
+
+const props = defineProps({
+  task: {
+    type: Object,
+    required: true,
+    default: {},
+  },
+})
+
+const store = useTaskStore()
+const { destroyTask } = store
 </script>
 
-
 <template>
-  <div class="bg-slate-800/30 hover:bg-slate-800/50 transition duration-200 hover:translate-x-1  rounded-2xl border border-slate-800 py-3 px-4">
+  <div
+    class="group bg-slate-800/30 hover:bg-slate-800/50 transition-all duration-200 hover:translate-x-1 rounded-2xl border border-slate-800 py-6 px-5"
+  >
     <div class="flex gap-10 items-center">
-      <input type="checkbox">
-      <span>{{task.title}}</span>
-      <div class="flex ml-auto gap-3 items-center">
-        <span>{{task.energy}}</span>
-        <span>icon</span>
-        <button @click="">
-          <svg class="w-10 h-10 stroke-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 7H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10L7.70141 19.3578C7.87432 20.3088 8.70258 21 9.66915 21H14.3308C15.2974 21 16.1257 20.3087 16.2986 19.3578L18 10" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>            
+      <input type="checkbox" />
+      <span class="grow">{{ task.title }}</span>
+      <div class="flex items-center justify-center relative w-10">
+        <div
+          class="flex items-center gap-1 group-hover:opacity-0 group-hover:invisible transition-all duration-200"
+        >
+          <span>{{ task.energy }}</span>
+          <span>icon</span>
+        </div>
+        <button
+          class="rounded-lg text-slate-500 absolute opacity-0 group-hover:opacity-100 group-hover:visible flex items-center justify-center invisible transition-all duration-200 hover:bg-red-200/20 hover:text-red-500/90 p-3"
+          @click="destroyTask(task)"
+        >
+          <svg
+            class="h-5 w-5 fill-current transition-colors duration-200"
+            viewBox="0 0 21 21"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.35,16 L9.45,16 L9.45,8 L7.35,8 L7.35,16 Z M11.55,16 L13.65,16 L13.65,8 L11.55,8 L11.55,16 Z M5.25,18 L15.75,18 L15.75,6 L5.25,6 L5.25,18 Z M7.35,4 L13.65,4 L13.65,2 L7.35,2 L7.35,4 Z M15.75,4 L15.75,0 L5.25,0 L5.25,4 L0,4 L0,6 L3.15,6 L3.15,20 L17.85,20 L17.85,6 L21,6 L21,4 L15.75,4 Z"
+              transform="translate(0, 0)"
+            />
+          </svg>
         </button>
       </div>
     </div>
   </div>
 </template>
 
-
-<style scoped>
-  
-</style>
+<style scoped></style>
