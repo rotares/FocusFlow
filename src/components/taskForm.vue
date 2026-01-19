8461042f2118d5215203489e60542a905e812ff7
Pavel Prigoda <rotares@disroot.org>
2026-01-19 14:06:45 +0300

    feat(taskForm): add basic validation and overflow check before create task

--- src/components/taskForm.vue
+++ src/components/taskForm.vue
@@ -1,31 +1,40 @@
 <script setup>
-import { reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
 import { useTaskStore } from '@/stores/task'
+import { storeToRefs } from 'pinia'
 
 const store = useTaskStore()
+const powerNums = [1, 2, 3, 4, 5]
 
 const newTask = reactive({
   title: '',
   energy: 1,
 })
 
-const powerNums = [1, 2, 3, 4, 5]
+const { getCurrentPower } = storeToRefs(store)
+const { storeTask, maxPower } = store
 
-const { storeTask } = store
+const onSumbit = () => {
+  storeTask(newTask)
+  Object.assign(newTask, { title: '', energy: 1 })
+}
+
+const isOverflowed = computed(() => getCurrentPower.value + newTask.energy > maxPower)
 </script>
 
 <template>
-  <div class="fixed bottom-0 left-0 right-0 max-w-2xl mx-auto px-6">
+  <div class="fixed bottom-0 left-0 right-0 p-6 bg-linear-to-t from-slate-900 via-[#0f172a] to-transparent">
     <div
-      class="focus-within:border-cyan-500/50 px-6 py-8 mb-10 border bg-slate-900 border-slate-800 rounded-2xl duration-300 transition-all"
+      class="max-w-3xl mx-auto"
     >
+    <div class="focus-within:border-cyan-500/50 px-6 py-7 mb-2 border bg-slate-900 border-slate-800 rounded-2xl duration-300 transition-all">
       <input
         v-model="newTask.title"
         type="text"
-        class="w-full px-1 py-1 outline-none placeholder:text-slate-400 text-slate-100"
+        class="w-full h-11 outline-none placeholder:text-slate-400 text-slate-100"
         placeholder="What are you focusing today?"
       />
-      <div class="flex justify-between pt-5">
+      <div class="flex justify-between pt-3">
         <div class="flex items-center bg-slate-800/50 p-1 rounded-lg border border-slate-700/50">
           <button
             @click="newTask.energy = powerNum"
@@ -33,32 +42,28 @@ const { storeTask } = store
               newTask.energy === powerNum
                 ? 'bg-cyan-500 text-white shadow-lg shadow-cyan-500/20'
                 : 'text-slate-500 hover:text-slate-300',
-              'px-3 py-1 rounded-md cursor-pointer transition-all duration-200',
+              'h-11 md:h-9.5 aspect-square rounded-md cursor-pointer transition-all duration-200',
             ]"
             v-for="powerNum in powerNums"
+            :key="powerNum"
           >
             {{ powerNum }}
           </button>
         </div>
         <button
-          @click="
-            () => {
-              storeTask({ ...newTask })
-              newTask.title = ''
-              newTask.energy = 1
-            }
-          "
+          @click="onSumbit()"
           :class="[
-            !newTask.title
+            !newTask.title || isOverflowed
               ? 'bg-slate-800 text-slate-600 cursor-not-allowed'
               : 'bg-white text-slate-950 hover:bg-cyan-400 transition-colors  cursor-pointer',
-            'px-4 py-2 rounded-lg transition-all duration-200',
+            'px-4 py-3 rounded-lg transition-all duration-200',
           ]"
-          :disabled="!newTask.title"
+          :disabled="!newTask.title || isOverflowed"
         >
           Добавить
         </button>
       </div>
+    </div>
     </div>
   </div>
 </template>
