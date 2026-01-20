c85ee9eb0df54eb6a9ce7f8f001486b4ee89e6f6
Pavel Prigoda <rotares@disroot.org>
2026-01-20 15:00:39 +0300

    fix(TaskList): change name of TaskItem component in template

--- src/components/taskList.vue
+++ src/components/taskList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import taskItem from '@/components/taskItem.vue'
+import TaskItem from '@/components/TaskItem.vue'
 import { useTaskStore } from '@/stores/task'
 import { storeToRefs } from 'pinia'
 
@@ -9,7 +9,7 @@ const { taskItems } = storeToRefs(store)
 
 <template>
   <div v-if="taskItems.length > 0" class="max-w-2xl mx-auto flex flex-col gap-5 pb-45">
-    <taskItem v-for="task in taskItems" :task="task" :key="task.id"></taskItem>
+    <TaskItem v-for="task in taskItems" :task="task" :key="task.id"></TaskItem>
   </div>
 </template>
 
