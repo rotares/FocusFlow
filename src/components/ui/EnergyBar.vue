025b108dc4cbac5f97a6fc74b1e93c370d6ce40e
Pavel Prigoda <rotares@disroot.org>
2026-01-20 14:58:29 +0300

    refactor(EnergyBar): extract energyBar from App.vue

---
+++ src/components/ui/EnergyBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useColorEnergy } from '@/composables/useColorEnergy'
+const { energyPercentage, gradientBar } = useColorEnergy()
+</script>
+
+<template>
+  <div class="h-0.75 bg-slate-400/20">
+    <div
+      :style="{ width: `${energyPercentage}%` }"
+      :class="[gradientBar, 'bg-linear-to-r transition-all duration-700 h-full']"
+    ></div>
+  </div>
+</template>
+
+<style scoped></style>
