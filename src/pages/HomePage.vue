ae85610c80797bcaeba7b1624f9a0b30ef3fd0fa
Pavel Prigoda <rotares@disroot.org>
2026-01-22 13:25:41 +0300

    feat(HomePage): add current date

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,14 +2,29 @@
 import TaskList from '@/components/TaskList.vue'
 import TaskForm from '@/components/TaskForm.vue'
 import { useTaskPersistence } from '@/composables/useTaskPersistence'
+import { Clock } from 'lucide-vue-next'
 
 useTaskPersistence()
+
+const today = new Date().toLocaleDateString('en-EN', {
+  day: 'numeric',
+  month: 'short',
+})
 </script>
 
 <template>
-  <div class="px-6 py-12">
-    <TaskList></TaskList>
-  </div>
+  <section class="px-6 py-8">
+    <div class="max-w-2xl mx-auto">
+      <div class="flex justify-between pb-5 text-slate-500">
+        <h2 class="text-base">Your Tasks</h2>
+        <div class="flex items-center justify-center gap-2 text-xs">
+          <Clock size="22" />
+          <p>{{ today }}</p>
+        </div>
+      </div>
+      <TaskList></TaskList>
+    </div>
+  </section>
   <TaskForm></TaskForm>
 </template>
 
