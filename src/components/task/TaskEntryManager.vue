429ef899b4f92f9fde8c41e9679c42f98f2ae9e9
Pavel Prigoda <rotares@disroot.org>
2026-01-25 19:44:16 +0300

    refactor(TaskEntryManager): reorganize component structure using useDevice composable

--- src/components/task/TaskEntryManager.vue
+++ src/components/task/TaskEntryManager.vue
@@ -3,22 +3,22 @@ import { ref } from 'vue'
 import TaskFormFields from './TaskFormFields.vue'
 import TaskModal from './TaskModal.vue'
 import AppButton from '@/components/base/AppButton.vue'
+import { useDevice } from '@/composables/useDevice'
 const showModal = ref(false)
+const { isMobile } = useDevice()
 </script>
 
 <template>
   <div
     class="fixed bottom-0 left-0 right-0 p-6 bg-linear-to-t from-slate-900 via-[#0f172a] to-transparent"
   >
-    <div class="hidden md:block">
-      <TaskFormFields />
-    </div>
-    <div class="md:hidden max-w-3xl mx-auto flex">
+    <div v-if="isMobile" class="max-w-3xl mx-auto flex">
       <AppButton v-if="!showModal" :variant="'active'" class="px-12 py-4" @click="showModal = true">
         +
       </AppButton>
       <TaskModal v-model="showModal" />
     </div>
+    <TaskFormFields v-else />
   </div>
 </template>
 
